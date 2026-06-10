fa67745de498a44923512c90691472aa126b2090
robertgimenofp-eng <robertgimenofp@ibf.cat>
2026-06-10 11:09:46 +0200

    feat: add Seguir comprando link to checkout page

--- views/carrito/checkout.php
+++ views/carrito/checkout.php
@@ -1,5 +1,12 @@
 <div class="container py-5 checkout-container" style="max-width: 800px;">
-    
+    <div class="mb-4">
+        <a href="index.php" class="text-decoration-none text-dark fw-bold">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left me-1" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+            </svg>
+            Seguir comprando
+        </a>
+    </div>
     <div class="text-center mb-5">
         <h2 class="fw-bold" style="font-size: 2.5rem; color: #000;">Tu pedido VivaEATS 🍔</h2>
     </div>
