679bfa0c45966252344755e27d9574aa53244b71
robertgimenofp-eng <robertgimenofp@ibf.cat>
2026-06-10 12:53:08 +0200

    chore: finalize docker config

--- database/db.php
+++ database/db.php
@@ -2,17 +2,26 @@
 
 class Database
 {
-    private static $host = "localhost";
-    private static $db = "restaurante_app";
+    // private static $host = "localhost";
+    // private static $db = "restaurante_app";
+    // private static $user = "root";
+    // private static $password = "";
+    // private static $port = 3306;
+    // private static $charset = "utf8mb4";
+
+    //====USO DOCKER====
+    private static $host = "127.0.0.1";
     private static $user = "root";
-    private static $password = "";
+    private static $password = "root";
+    private static $db = "restaurante_app";
+    private static $port = 3307;
     private static $charset = "utf8mb4";
 
     public static function connect()
     {
         try {
             $pdo = new PDO(
-                "mysql:host=" . self::$host . ";dbname=" . self::$db . ";charset=" . self::$charset,
+                "mysql:host=" . self::$host . ";port=" . self::$port . ";dbname=" . self::$db . ";charset=" . self::$charset,
                 self::$user,
                 self::$password
             );
